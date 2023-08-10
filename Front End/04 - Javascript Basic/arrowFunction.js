842db2b73a26db7bd4083917903785f81863acaa
yasir-rtx <yasir112358@gmail.com>
2023-08-10 16:16:21 +0700

    Class (Javascript) : arrow function

---
+++ Front End/04 - Javascript Basic/arrowFunction.js
@@ -0,0 +1,17 @@
+// Common function expression
+// const random = function() {
+//     return Math.floor(Math.random() * 6 + 1);
+// }
+
+// Arrow Function
+// const random = () => {
+//     return Math.floor(Math.random() * 6 + 1);
+// }
+
+// variation if it's simple task
+// const random = () => (
+//     Math.floor(Math.random() * 6 + 1)
+// );
+
+const random = () => (Math.floor(Math.random() * 6 + 1))
+const add = (a,b) => a + b;

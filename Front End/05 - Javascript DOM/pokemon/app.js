f2e9df4376ada0b591561df104c752fb83c62041
yasir <yelfvankarlenstain@gmail.com>
2023-08-15 14:49:59 +0700

    Class (Javascript DOM) : Latihan Pokemon Selesai

--- Front End/05 - Javascript DOM/pokemon/app.js
+++ Front End/05 - Javascript DOM/pokemon/app.js
@@ -1,10 +1,9 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const baseImgURL =
-	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const baseImgURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i <= 500; i++) {
+for (let i = 1; i <= 1000; i++) {
 	const pokeBall = document.createElement('div');
 	pokeBall.classList.add('pokemon');
 

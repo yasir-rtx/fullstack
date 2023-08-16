21cff783da430daf352c9ed29d172e6607c31332
yasir-rtx <yasir112358@gmail.com>
2023-08-16 11:31:33 +0700

    Class (Javascript DOM) : Event input dan change

--- Front End/05 - Javascript DOM/event/preventDefault/script.js
+++ Front End/05 - Javascript DOM/event/preventDefault/script.js
@@ -2,13 +2,28 @@ const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const list = document.querySelector('#notes');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // mencegah behaviour dari suatu elemen
+// Event Change => mendeteksi perubagan pada bagian input
+// input.addEventListener('change', (e) => {
+//     console.log('Terjadi Perubahan');
+// });
 
-    const noteValue = input.value; // properti value berfungsi mengambil value dari form
-    const newList = document.createElement('li');
-    newList.innerText = noteValue;
-    // console.log(newList);
-    list.appendChild(newList);
-    input.value = '';
+// Event Input => Mendeteksi setiap perubahan yang terjadi pada form / menghitung setiap karakter yang masuk ke input
+// input.addEventListener('input', (e) => {
+//     // document.querySelector('h1').innerText = input.value;
+//     console.log('New Input');
+// });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (input.value !== '') {
+        console.log('Process input');
+        const noteValue = input.value;
+        const newList = document.createElement('li');
+        newList.innerText = noteValue;
+        list.appendChild(newList);
+        input.value = '';
+    } else {
+        console.log('Empty Input');
+    }
 });

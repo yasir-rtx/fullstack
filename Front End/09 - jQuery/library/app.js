d4e23a54e50d9989f31684279cb7d1f71cfdfb0b
yasir-rtx <yasir112358@gmail.com>
2023-09-11 15:00:28 +0700

    Class (jQuery) : library

---
+++ Front End/09 - jQuery/library/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $(".img-slider").slick({
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: true,
+            dots: true,
+            centermode: true,
+            fade: true,
+            cssEase: "linear",
+        });
+    });
+});
